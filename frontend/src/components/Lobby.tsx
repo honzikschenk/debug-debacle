077de06785ee14cfbf819abf8ff1b7b02f585b94
honzikschenk <nojschenk@gmail.com>
2025-01-25 18:55:31 -0500

    Implement toast notifications for lobby code copying and add leave game functionality

--- frontend/src/components/Lobby.tsx
+++ frontend/src/components/Lobby.tsx
@@ -1,5 +1,6 @@
 import { Link, User } from "lucide-react";
 import { Button } from "./ui/button";
+import { useToast } from "@/hooks/use-toast"
 
 interface LobbyProps {
   id: number;
@@ -9,12 +10,22 @@ interface LobbyProps {
 
 const Lobby = ({ id, onStart, players }: LobbyProps) => {
 
+  const { toast } = useToast();
+
+  const copyCode = (): void => {
+    navigator.clipboard.writeText(id.toString())
+    toast({
+        title: "Code Copied!",
+        description: "You can now share the code with your friends."
+      })
+  }
+
   return (
     <div className="absolute backdrop-blur-md z-20 w-screen h-screen flex items-center justify-center text-white">
       <div className="max-w-3xl max-h-96 flex items-center flex-col gap-y-2">
         <h2 className="flex font-semibold text-3xl">Lobby #{id}</h2>
         <div className="flex items-center"><User className="h-4 w-4" /> {players.length}: <span className="text-sm ml-2">{players.join(', ')}</span></div>
-        <Button variant="secondary" className="flex items-center gap-x-2">
+        <Button onClick={copyCode} variant="secondary" className="flex items-center gap-x-2">
           Copy Link
         </Button>
         <Button onClick={onStart} variant="secondary" className="flex items-center gap-x-2 bg-green-500 hover:bg-green-600">
