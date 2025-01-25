7a288121bc96ad4f49cc679087c831e120b7845c
honzikschenk <nojschenk@gmail.com>
2025-01-25 14:53:24 -0500

    Adds Auth0Provider component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
@@ -12,7 +13,15 @@ const basename = import.meta.env.BASE_URL;
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
-      <App />
+      <Auth0Provider
+        domain="dev-qgsk4elrtd81iff0.us.auth0.com"
+        clientId="6kx3j26X5iKw120rB7m7hwapVQV1IMUK"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
